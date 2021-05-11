d99a9341878736f19c12df3926e96fd9c8d1caa4
Thalita Teixeira <thalitavst@gmail.com>
2021-05-11 13:41:22 -0300

    Finalizando as alterações e resolução do desafio da calculadora

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -4,12 +4,62 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question("Qual o valor investido ? ", function (valor) {
-  console.log(`${valor}`);
-  rl.close();
-});
+// 30 pessoas visualizam o anúncio original (não compartilhado) a cada R$ 1,00 investido.
+function obterNumeroTotalVisualizacaoPorValorInvestido(valorInvestido) {
+  return (numeroTotalVisualizacaoPorValorInvestido = valorInvestido * 30);
+}
+
+// a cada 100 pessoas que visualizam o anúncio 12 clicam nele.
+function obterNumeroCliques(total) {
+  return (12 / 100) * total;
+}
+
+// a cada 20 pessoas que clicam no anúncio 3 compartilham nas redes sociais.
+function obterNumeroCompartilhamento(total) {
+  return (3 / 20) * total;
+}
+
+// cada compartilhamento nas redes sociais gera 40 novas visualizações.
+function obterNumeroVisualizacoesPorCompartilhamento(total) {
+  return total * 40;
+}
+
+// Imprime o resultado no console
+function imprimirResultado(resultado) {
+  console.log(`O número estimado é de ${resultado} visualizações`);
+}
+
+// o mesmo anúncio é compartilhado no máximo 4 vezes em sequência
+function obterTotalPorCompartilhamento(totalAnuncio) {
+  return totalAnuncio * 4;
+}
+
+function start() {
+  rl.question("Qual o valor investido ? ", function (valor) {
+    if (valor < 1) {
+      console.log(`${valor} abaixo do permitido.`);
+      rl.close();
+    }
+    let numeroTotalVisualizacaoPorValorInvestido = obterNumeroTotalVisualizacaoPorValorInvestido(
+      valor
+    );
+    let numeroClique = obterNumeroCliques(
+      numeroTotalVisualizacaoPorValorInvestido
+    );
+    let numeroCompartilhamento = obterNumeroCompartilhamento(numeroClique);
+    let numeroVisualizacaoPorCompartilhamento = obterNumeroVisualizacoesPorCompartilhamento(
+      numeroCompartilhamento
+    );
+
+    imprimirResultado(Math.round(numeroVisualizacaoPorCompartilhamento));
+
+    rl.close();
+  });
+}
 
 rl.on("close", function () {
   console.log("\nBYE BYE !!!");
   process.exit(0);
 });
+
+start();
